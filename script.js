a56764ead2573fe7b4fd9404853aedc9fe2d7cff
Lucas AmorimPorciuncula <lucas.porciuncula@al.infnet.edu.br>
2024-04-21 12:04:21 -0300

    formula para determiar sua idade atual

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-anoNascimento = 2004
 
-idade = 2024 - anoNascimento
+anoNascimento = 2003
+hoje = new Date()
 
+
+anoAtual = hoje.getFullYear()
+idade = anoAtual - anoNascimento
 nome = "Lucas"
-
 mensagem = "Sou " + nome + " e tenho " + idade + " anos"
 
 alert(mensagem)
+
